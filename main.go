4434bcedf269411c91420676157c15a4a967d917
heroineyy <93510710@qq.com>
2024-06-19 18:13:39 +0800

    1

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"byte_douyin_project/config"
+	"byte_douyin_project/router"
+	"fmt"
+)
+
+func main() {
+	r := router.InitDouyinRouter()
+	err := r.Run(fmt.Sprintf(":%d", config.Info.Port)) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	if err != nil {
+		return
+	}
+}
